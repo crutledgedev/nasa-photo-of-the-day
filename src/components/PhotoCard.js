dd5022afddb89322a8d79bd4cf6df468822345f1
Chance Rutledge <chancerutledge2@gmail.com>
2019-11-07 20:33:44 -0800

    styled components added

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,25 +1,35 @@
 import React from 'react';
+import Styled from 'styled-components';
 import {
   Card, CardImg, CardText, CardBody, CardLink,
   CardTitle, CardSubtitle
 } from 'reactstrap';
 
+const ContentContainer = Styled.div`
+  max-width: 60%;
+  margin: 0 auto;
+  font-size: 1.5rem;
+`;
+const Descriptor = Styled.p`
+font-size: 1.2rem;
+`;
+{/* <CardText> */}
 const PhotoCard = (props) => {
   return (
-    <div>
+    <ContentContainer>
       <Card>
         <CardBody>
-          <CardTitle>{props.title}</CardTitle>
-          <CardSubtitle>Copyright: {props.credit}</CardSubtitle>
+          <CardTitle>{props.title} </CardTitle>
         </CardBody>
         <img width="100%" src={props.picture} alt="Astronomy Picture" />
         <CardBody>
-          <CardText>{props.description}}</CardText>
+        <CardSubtitle>Copyright: {props.credit}, {props.date}</CardSubtitle>
+          <Descriptor>{props.description}</Descriptor>
           <CardLink href="https://apod.nasa.gov/apod/archivepix.html">Discover the Universe</CardLink>
           <CardLink href="https://hubblesite.org/">HubbleSite.org</CardLink>
         </CardBody>
       </Card>
-    </div>
+    </ContentContainer>
   );
 };
 
